88758e410e762b4e698f494961a04045808a3377
hector Dountuo <hectordountuo@yahoo.com>
2022-12-01 22:08:36 +0100

    creation page home

---
+++ jeux/PHP/home.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<link rel="stylesheet" href="CSS/home.css">
+    <title></title>
+</head>
+<body>
+    <div class="flex">
+        <div class="bloc1"></div>
+        <div class="bloc2"></div>
+        <div class="bloc3"></div>
+</div>
+
+
+</body>
+</html>
