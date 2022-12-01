038a7fd7887838d39aa1ea2811bea7e0afe03123
hector Dountuo <hectordountuo@yahoo.com>
2022-12-01 22:47:05 +0100

    creation des blocs

--- jeux/PHP/home.php
+++ jeux/PHP/home.php
@@ -1,13 +1,17 @@
 <html>
 <head>
-<link rel="stylesheet" href="CSS/home.css">
+<link rel="stylesheet" href="/jeux/CSS/home.css">
     <title></title>
 </head>
 <body>
     <div class="flex">
-        <div class="bloc1"></div>
-        <div class="bloc2"></div>
-        <div class="bloc3"></div>
+        <div class="bloc1">
+            <h1>Labyrinthe</h1>
+        </div>
+        <div class="bloc2">
+                <h1>Nuit de l'info</h1>
+        </div>
+        <div class="bloc3">     <h1>les jeux</h1></div>
 </div>
 
 
