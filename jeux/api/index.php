d6715f34ae29cf0b3546a30874cc858c1b7082ff
gregoirecesi <gregoire.blanchard@viacesi.fr>
2022-12-02 02:24:27 +0100

    api

--- jeux/api/index.php
+++ jeux/api/index.php
@@ -2,10 +2,27 @@
 
 $response = file_get_contents('https://geo.api.gouv.fr/regions?fields=nom,code');
 $response = json_decode($response);
-foreach($response as $row)
-{
-    echo $row->nom;
-    echo "</br>";
-}
 
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Régions de la france</title>
+</head>
+<body>
+    <h1>Régions</h1>
+    <div>
+        <?php
+            foreach($response as $row)
+            {
+                echo $row->nom;
+                echo "</br>";
+            }
+        ?>
+    </div>
+</body>
+</html>
