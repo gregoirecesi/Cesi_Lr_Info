795a3e06ce59a67aec500fef21e74845c2b3bcbe
PoTrd <paul.taurand@gmail.com>
2022-12-02 04:14:15 +0100

    finito

--- jeux/PHP/home.php
+++ jeux/PHP/home.php
@@ -1,13 +1,17 @@
 <html>
 <head>
-<link rel="stylesheet" href="/jeux/CSS/home.css">
+<link rel="stylesheet" href="../CSS/home.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow&family=Roboto+Condensed&display=swap" rel="stylesheet">
+</head>
     <title></title>
 </head>
 <body>
     <div class="flex">
-        <div class="bloc1">    <h1 class="titres">Labyrinthe</h1>     </div>
-        <div class="bloc2">    <h1 class="titres">La Nuit de l'info</h1>     </div>
-        <div class="bloc3">    <h1 class="titres">Test de Pureté</h1>     </div>
+        <div class="bloc1">    <h1 class="titre_lab">Labyrinthe</h1>     </div>
+        <div class="bloc2">    <h1 class="titre_NI">La Nuit de l'info</h1>     </div>
+        <div class="bloc3">    <h1 class="titre_TP">Test de Pureté</h1>     </div>
         
 </div>
 
