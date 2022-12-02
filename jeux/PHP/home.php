533fcb5857e436a9938993f4dce62c5ea507ae0a
PoTrd <paul.taurand@gmail.com>
2022-12-02 03:01:09 +0100

    page accueil et titre

--- jeux/PHP/home.php
+++ jeux/PHP/home.php
@@ -5,9 +5,9 @@
 </head>
 <body>
     <div class="flex">
-        <div class="bloc1"></div>
-        <div class="bloc2"></div>
-        <div class="bloc3"></div>
+        <div class="bloc1">    <h1 class="titres">Labyrinthe</h1>     </div>
+        <div class="bloc2">    <h1 class="titres">La Nuit de l'info</h1>     </div>
+        <div class="bloc3">    <h1 class="titres">Test de Pureté</h1>     </div>
         
 </div>
 
